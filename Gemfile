8dab22dc41f4fc2205ac2f7d3346dbe7434fb690
nashikotokyo <natsumi.creo@gmail.com>
2021-12-22 21:55:58 +0900

    [Add] Gem 'rails-i18n'の導入と基本設定

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'jquery-rails'
 gem 'popper_js'
 gem 'redis-rails'
 gem 'annotate'
+gem 'rails-i18n', '~> 5.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module InstaCloneByMe
 
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
