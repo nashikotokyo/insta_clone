7bd007325810192a4f0a7265e468bedd297e8119
nashikotokyo <natsumi.creo@gmail.com>
2022-01-31 11:40:08 +0900

    [Add] seedファイルの作成

---
+++ db/seeds/posts.rb
@@ -0,0 +1,8 @@
+puts 'Start inserting seed "posts" ...'
+User.limit(10).each do |user|
+  post = user.posts.create!(
+    body: Faker::Hacker.say_something_smart,
+    images: File.open("#{Rails.root}/db/fixtures/dummy.png")
+  )
+  puts "post#{post.id} has created!"
+end
