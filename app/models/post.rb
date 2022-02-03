a132a1466d7b8d9b326156c23178cb82291c7707
nashikotokyo <natsumi.creo@gmail.com>
2022-02-03 16:38:06 +0900

    [Add] CarrierWave アップローダークラスの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,6 +19,7 @@
 #
 class Post < ApplicationRecord
   belongs_to :user
+  mount_uploaders :images, PostImageUploader
 
   validates :images, presence: true
   validates :body, presence: true, length: {maximum: 1000}
