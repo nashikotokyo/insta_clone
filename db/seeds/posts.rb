f987b4982d4f9ede5c04ecd418d96c8069acc4ad
nashikotokyo <natsumi.creo@gmail.com>
2022-02-03 22:40:48 +0900

    [Modify] postsのseedファイル内の画像の設定を配列形式[]に直した

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -2,7 +2,7 @@ puts 'Start inserting seed "posts" ...'
 User.limit(10).each do |user|
   post = user.posts.create!(
     body: Faker::Hacker.say_something_smart,
-    images: File.open("#{Rails.root}/db/fixtures/dummy.png")
+    images: [File.open("#{Rails.root}/db/fixtures/dummy.png")]
   )
   puts "post#{post.id} has created!"
 end
