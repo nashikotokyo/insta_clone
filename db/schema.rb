317b83872df0d6248a5a2564f7b269f5823cc5a7
nashikotokyo <natsumi.creo@gmail.com>
2022-02-03 15:08:06 +0900

    [Add] PostテーブルとPostモデルの作成とアソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_063011) do
+ActiveRecord::Schema.define(version: 2022_02_03_023838) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.string "images", null: false
+    t.text "body", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_12_14_063011) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
