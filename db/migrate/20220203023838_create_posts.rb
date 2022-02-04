5185cab7525613d78bdd7812916384506173b369
nashikotokyo <natsumi.creo@gmail.com>
2022-02-04 16:48:38 +0900

    [Modify] imagesカラムをJSON型にしてcarrierwaveで複数画像扱えるよう対応

--- db/migrate/20220203023838_create_posts.rb
+++ db/migrate/20220203023838_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :images, null: false
+      t.json :images, null: false
       t.text :body, null: false
       t.references :user, foreign_key: true
 
