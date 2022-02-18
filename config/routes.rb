3ec4dd948efc49e1d1f3faf942cbb44accc1c54a
nashikotokyo <natsumi.creo@gmail.com>
2022-02-18 17:06:40 +0900

    [Modify] ルートパスとログイン後のリダイレクト先を変更

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@
 #      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'posts#index'
 
   resources :posts
   resources :users, only: %i[new create]
