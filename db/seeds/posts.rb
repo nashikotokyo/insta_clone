381c38b751262ec2d4f876c07769f85e5dc03407
nashikotokyo <natsumi.creo@gmail.com>
2022-02-21 16:51:31 +0900

    [Modify] seedのダミー画像をダウンロードしてくる形式に変更

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -2,7 +2,7 @@ puts 'Start inserting seed "posts" ...'
 User.limit(10).each do |user|
   post = user.posts.create!(
     body: Faker::Hacker.say_something_smart,
-    images: [File.open("#{Rails.root}/db/fixtures/dummy.png")]
+    remote_images_urls: %w[https://picsum.photos/350/350/?random https://picsum.photos/350/350/?random]
   )
   puts "post#{post.id} has created!"
 end
