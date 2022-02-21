7f415b3bd6707d0d7f0ce7bf6a40eb720551ccc0
nashikotokyo <natsumi.creo@gmail.com>
2022-02-21 18:30:10 +0900

    [Modify] bundle exec rubocop -a

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,5 +22,5 @@ class Post < ApplicationRecord
   mount_uploaders :images, PostImageUploader
 
   validates :images, presence: true
-  validates :body, presence: true, length: {maximum: 1000}
+  validates :body, presence: true, length: { maximum: 1000 }
 end
