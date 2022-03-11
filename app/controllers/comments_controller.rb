a5c8270d7df8dd5aa69a3e6ad030dc84d8a47772
nashikotokyo <natsumi.creo@gmail.com>
2022-03-11 12:20:34 +0900

    [Add] ログイン制御対応漏れ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :require_login, only: %i[create edit update destroy]
 
   def create
     @comment = current_user.comments.build(comment_params)
