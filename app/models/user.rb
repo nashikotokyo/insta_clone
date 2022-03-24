cc91972b59c8686653f34f83abe9e0d2e46412fa
nashikotokyo <natsumi.creo@gmail.com>
2022-03-24 15:12:11 +0900

    [Add] UserとRelationshipモデルのアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,15 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
-
+  has_many :active_relationships, class_name: 'Relationship',
+           foreign_key: 'follower_id',
+           dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship',
+           foreign_key: 'followed_id',
+           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower           
+  
   def own?(object)
     id == object.user_id
   end
