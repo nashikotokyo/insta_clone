d6f117cca905e0d43f214f37be1e887bcef87612
nashikotokyo <natsumi.creo@gmail.com>
2022-03-25 11:55:44 +0900

    [Add] フォローアンフォロー機能の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,4 +54,16 @@ class User < ApplicationRecord
   def like?(post)
     like_posts.include?(post)
   end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
