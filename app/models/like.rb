47db942cd6b9188ce43cdd19524f9022c1c463c2
nashikotokyo <natsumi.creo@gmail.com>
2022-04-21 11:16:59 +0900

    [Add] 通知機能の為のActivityモデル&テーブル作成、アソシエーション

--- app/models/like.rb
+++ app/models/like.rb
@@ -22,6 +22,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  has_one :activity, as: :subject, dependent: :destroy
 
   validates :user_id, uniqueness: { scope: :post_id }
 end
