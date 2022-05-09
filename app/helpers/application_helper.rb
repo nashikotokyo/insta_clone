4ee846e8d3dfc6c0725d65a025ce0be3fff1a434
nashikotokyo <natsumi.creo@gmail.com>
2022-05-09 23:12:49 +0900

    [Modify] be rubocop -a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,18 +7,18 @@ module ApplicationHelper
       description: Settings.meta.description,
       keywords: Settings.meta.keywords,
       canonical: request.original_url,
-        og: {
-          title: :full_title,
-          type: Settings.meta.og.type,
-          url: request.original_url,
-          image: image_url(Settings.meta.og.image_path),
-          site_name: :site,
-          description: :description,
-          locale: 'ja_JP'
-        },
-        twitter: {
-          card: 'summary_large_image'
-        },
+      og: {
+        title: :full_title,
+        type: Settings.meta.og.type,
+        url: request.original_url,
+        image: image_url(Settings.meta.og.image_path),
+        site_name: :site,
+        description: :description,
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image'
+      }
     }
   end
 end
