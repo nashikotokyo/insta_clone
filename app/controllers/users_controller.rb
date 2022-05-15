99b8650c1dfe05a3f2323c7dd246194921a4acff
nashikotokyo <natsumi.creo@gmail.com>
2022-05-15 10:10:47 +0900

    [Modify] ユーザ登録成功後投稿一覧画面に遷移するように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to new_user_path, success: 'ユーザーを作成しました'
+      redirect_to posts_path, success: 'ユーザーを作成しました'
     else
       flash.now[:danger] = 'ユーザーの作成に失敗しました'
       render :new
